1756df3d30b14b03527dd2b8bb31a4593d708b75
victoriaTamber <victoriatamber06@gmail.com>
2024-03-22 11:27:12 +0100

    commit 1

---
+++ kodeVSC_oppdragTolv/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <div>
+      <a href="inlogging.php">Innlogging:</a>
+      <a href="regBruker.php">Registrer ny bruker</a>
+    </div>
+</body>
+</html>
