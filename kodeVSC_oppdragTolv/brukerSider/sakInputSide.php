e7c54d817aa0da9ded84e9e687297fc0bf8b8406
victoriaTamber <victoriatamber06@gmail.com>
2024-04-02 17:09:48 +0200

    commit 10

--- kodeVSC_oppdragTolv/brukerSider/sakInputSide.php
+++ kodeVSC_oppdragTolv/brukerSider/sakInputSide.php
@@ -8,10 +8,10 @@
 <body>
     <h1>Fjell Bedriftløsninger sin ticketsystem </h1>
 <form action="sakInputSide.php">
-  <label for="navnInput">navn:</label>
+  <label for="navn_brukerInput">navn:</label>
   <input type="text" id="navn" name="navn"><br><br>
 
-  <label for="epostInput">E-post:</label>
+  <label for="epost_brukerInput">E-post:</label>
   <input type="email" id="epost" name="epostInput"><br><br>
 
   <label for="sakTittelInput" >tittel:</label>
@@ -30,10 +30,10 @@
 <?php
  include ("../db_kobling.php");
  
- $navn_input = $_POST['navn_bruker'];
- $epost_input = $_POST['epost_bruker'];
- $sak_tittel = $_POST['sak_tittel'];
- $sakBeskrivelse = $_POST['sak_beskrivelse'];
+ $navn_input = $_POST['navn_brukerInput'];
+ $epost_input = $_POST['epost_brukerInput'];
+ $sak_tittel = $_POST['sakTittelInput'];
+ $sakBeskrivelse = $_POST['sakBeskrivelseInput'];
 
 
 
@@ -49,10 +49,16 @@
 
 
 
- $sql = "SET FOREIGN_KEY_CHECKS=0";
- $result = mysql_query($conn, $sql);
+$sql = "SET FOREIGN_KEY_CHECKS=0";
+$result = mysqli_query($conn, $sql);
 
- $sql = "INSERT INTO oppdrag_uketolv.sak (sak_tittel, sak_beskrivelse, sak_dato) VALUES ('$sak_tittel', '$sakBeskrivelse', NOW())";
+
+ $sql = "INSERT INTO oppdrag_uketolv.bruker (navn_bruker, epost_bruker) VALUES ('$navn_input', '$epost_input')";
+
+ //henter bruker id fra bruker til sak
+ $idbruker = mysqli_insert_id($conn);
+
+ $sql = "INSERT INTO oppdrag_uketolv.sak (fk_idbruker, sak_tittel, sak_beskrivelse, sak_dato) VALUES ('$idbruker', '$sak_tittel', '$sakBeskrivelse', NOW())";
  $result = mysqli_query($conn, $sql) or die('Error querying database.');
 
 
